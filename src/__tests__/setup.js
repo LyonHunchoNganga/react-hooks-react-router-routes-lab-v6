ba4b89dcbae4321c06bc735b2871edbfaac353a6
lyonHunchoNganga <lyonnganga1@gmail.com>
2025-07-27 12:18:59 +0300

    commit

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,9 +1,2 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-import 'whatwg-fetch';
-import {Blob} from 'node:buffer';
-
-afterEach(() => {
-    cleanup();
-})
+import '@testing-library/jest-dom'
+import 'whatwg-fetch'
